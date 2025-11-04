c1634b4c010183830bb36d6866858229b43820b1
Nick Kosarev <hmbanan666@hotmail.com>
2025-11-04 17:48:56 +0200

    feat: header (#2)
    * feat: header
    
    * chore: update

--- apps/web-app/app/components/Header.vue
+++ apps/web-app/app/components/Header.vue
@@ -1,3 +1,57 @@
 <template>
-  k39
+  <header class="bg-default border-b border-default sticky h-16 top-0 z-50">
+    <UContainer class="flex items-center justify-between gap-3 h-full">
+      <div class="lg:flex-1 flex items-center gap-1.5">
+        <UButton
+          icon="i-lucide-menu"
+          color="neutral"
+          variant="outline"
+          size="xl"
+          class="visible lg:hidden"
+          @click="isMobileMenuOpened = true"
+        />
+
+        <!-- <Logo /> -->
+      </div>
+
+      <div class="hidden lg:flex">
+        <UNavigationMenu
+          :items="mainNavigationItems"
+          variant="link"
+        />
+      </div>
+
+      <div class="flex items-center justify-end lg:flex-1 gap-2.5">
+        <div class="hidden lg:flex items-center justify-end lg:flex-1 gap-1.5">
+          <UColorModeButton size="xl" />
+          <GithubButton />
+        </div>
+      </div>
+    </UContainer>
+  </header>
+
+  <USlideover
+    v-model:open="isMobileMenuOpened"
+    side="left"
+  >
+    <template #header>
+      <div class="flex flex-row items-center">
+        <UButton
+          icon="i-lucide-x"
+          color="neutral"
+          variant="outline"
+          size="xl"
+          @click="isMobileMenuOpened = false"
+        />
+      </div>
+    </template>
+
+    <template #body>
+      <Navigation />
+    </template>
+  </USlideover>
 </template>
+
+<script setup lang="ts">
+const { isMobileMenuOpened, mainNavigationItems } = useApp()
+</script>
