7cd34c359889ac6e0f7bee44467092a4a0092543
Nick Kosarev <hmbanan666@hotmail.com>
2025-11-11 11:16:40 +0200

    chore: photo block

--- apps/web-app/app/components/page/ReviewCard.vue
+++ apps/web-app/app/components/page/ReviewCard.vue
@@ -54,6 +54,16 @@
         title="Комментарий"
         :content="review.comment"
       />
+
+      <div class="flex flex-col gap-2">
+        <p class="text-base/5 font-bold">
+          Фото
+        </p>
+
+        <div class="flex flex-row gap-2">
+          <img src="/img/page-avatar/muza-avatar.jpg" class="size-28 rounded-lg border border-default">
+        </div>
+      </div>
     </div>
 
     <div class="flex flex-row gap-2 justify-between items-center">
